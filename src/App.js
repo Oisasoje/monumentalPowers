22271f288c9ac5017440011a3da1a314349025e1
Oisasoje <ohikhemotaoovictor@gmail.com>
2025-03-21 09:18:05 +0100

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employees';
 
 function App() {
+  const showEmployee = true
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       { showEmployee ?
+
+    <>
+<Employee />
+<Employee />
+</>
+: <p>Cannot view employees</p>
+       }
+
     </div>
   );
 }
